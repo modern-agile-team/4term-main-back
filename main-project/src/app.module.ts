8f432841e8bcb0dc4299b6b2c60cc956c6985e80
JUHA <khabh@naver.com>
2022-08-30 22:03:33 +0900

    Add(juha/config): @nestjs/config 이용해서 typeorm 설정

--- main-project/src/app.module.ts
+++ main-project/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './common/configs/typeorm.config';
 import { MeetingsModule } from './meetings/meetings.module';
 
 @Module({
-  imports: [MeetingsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+    MeetingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
