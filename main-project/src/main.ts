4d902dbfe8d851a84cf3e6c8489e5e0996e635f4
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-08-31 14:30:44 +0900

    Add(minho/board): board 파일구조 생성, @nestjs/schematics 모듈 설치

--- main-project/src/main.ts
+++ main-project/src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
+
   await app.listen(3000);
+  Logger.debug(`서버 가동 port : ${port}`);
 }
 bootstrap();
