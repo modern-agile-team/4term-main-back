39dd12c474a8d24a219773ac878cece6c475335c
JUHA <khabh@naver.com>
2022-09-01 17:25:15 +0900

    Add(juha/meeting): members, meetings 엔티티 관계 설정 #1

--- main-project/src/meetings/repository/meeting.repository.ts
+++ main-project/src/meetings/repository/meeting.repository.ts
@@ -1,9 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { MeetingInfo } from '../entity/meeting-info.entity';
 import { Meeting } from '../entity/meeting.entity';
 
 @EntityRepository(Meeting)
 export class MeetingRepository extends Repository<Meeting> {}
-
-@EntityRepository(MeetingInfo)
-export class MeetingInfoRepository extends Repository<MeetingInfo> {}
