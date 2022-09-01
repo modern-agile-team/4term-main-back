cb9d9520f8a80a8693df3f1854fe4f70eea2482b
JUHA <khabh@naver.com>
2022-09-01 14:59:47 +0900

    Add(juha/member): host,guest member 엔티티 생성 #1

--- main-project/src/members/members.module.ts
+++ main-project/src/members/members.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  GuestMembersRepository,
+  HostMembersRepository,
+} from './repository/members.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([HostMembersRepository, GuestMembersRepository]),
+  ],
   providers: [MembersService],
-  controllers: [MembersController]
+  controllers: [MembersController],
 })
 export class MembersModule {}
