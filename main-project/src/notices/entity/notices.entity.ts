3fb46175d8d12dc07ab48768dbe5c1049cc7ecbb
JUHA <khabh@naver.com>
2022-09-02 09:46:28 +0900

    Add(juha/notice): notices 엔티티 생성 # 18

---
+++ main-project/src/notices/entity/notices.entity.ts
@@ -0,0 +1,31 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('notices')
+export class Notices extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  no: number;
+
+  @Column({ name: 'user_no' })
+  userNo: number;
+
+  @Column({ name: 'target_user_no', nullable: true })
+  targetUserNo: number;
+
+  @Column()
+  type: number;
+
+  @Column({ name: 'read_datetime', nullable: true })
+  readDatetime: Date;
+
+  @CreateDateColumn()
+  datetime: Date;
+
+  @Column('varchar', { length: 100, nullable: true })
+  value: string;
+}
