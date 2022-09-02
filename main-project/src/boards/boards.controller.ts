6c706cbb1fbab62f84a4823d12ef93457b00b04a
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-09-02 16:28:38 +0900

    Feature(minho/board): 게시글 상세조회 #4

--- main-project/src/boards/boards.controller.ts
+++ main-project/src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { Board } from './entity/board.entity';
@@ -7,11 +7,19 @@ import { Board } from './entity/board.entity';
 export class BoardsController {
   private logger = new Logger('BoardsController');
   constructor(private boardService: BoardsService) {}
+
   @Get()
   getAllBoards(): Promise<Board[]> {
     this.logger.debug(`Get all boards.`);
 
     return this.boardService.getAllBoards();
+  }
+
+  @Get('/:boardNo')
+  getBoardByNo(@Param('boardNo') boardNo: number): Promise<Board> {
+    this.logger.debug(`Get board by boardNo.`);
+
+    return this.boardService.getBoardByNo(boardNo);
   }
 
   @Post()
--- main-project/src/boards/boards.service.ts
+++ main-project/src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { Board } from './entity/board.entity';
@@ -11,11 +11,20 @@ export class BoardsService {
     private boardRepository: BoardRepository,
   ) {}
 
-  createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardRepository.createBoard(createBoardDto);
   }
 
-  getAllBoards(): Promise<Board[]> {
+  async getAllBoards(): Promise<Board[]> {
     return this.boardRepository.find();
+  }
+
+  async getBoardByNo(boardNo: number): Promise<Board> {
+    const found = await this.boardRepository.findOne(boardNo);
+
+    if (!found) {
+      new NotFoundException(`Can't find Board with boardNo ${boardNo}`);
+    }
+    return found;
   }
 }
