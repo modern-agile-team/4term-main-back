faae9b166c9f7ee5406cd6a03cb99c9c4bec5e3a
JUHA <khabh@naver.com>
2022-09-08 15:28:23 +0900

    Refactor(juha/meeting): import문 삭제 #1

--- main-project/src/meetings/meetings.service.ts
+++ main-project/src/meetings/meetings.service.ts
@@ -4,6 +4,7 @@ import { GuestMembersRepository } from 'src/members/repository/guest-members.rep
 import { HostMembersRepository } from 'src/members/repository/host-members.repository';
 import { CreateMeetingDto } from './dto/createMeeting.dto';
 import { UpdateMeetingDto } from './dto/updateMeeting.dto';
+import { Meetings } from './entity/meeting.entity';
 import { MeetingInfoRepository } from './repository/meeting-info.repository';
 import { MeetingRepository } from './repository/meeting.repository';
 
@@ -28,7 +29,7 @@ export class MeetingsService {
     private readonly hosttMembersRepository: HostMembersRepository,
   ) {}
 
-  async createMeeting(createMeetingDto: CreateMeetingDto): Promise<object> {
+  async createMeeting(createMeetingDto: CreateMeetingDto): Promise<Meetings> {
     try {
       const { location, time, host } = createMeetingDto;
       const { affectedRows, insertId } =
@@ -49,10 +50,8 @@ export class MeetingsService {
         values.push({ meetingNo: meeting.no, userNo });
         return values;
       }, []);
-
       const addHostInfoResult =
         await this.hosttMembersRepository.addHostMembers(hostsInfo);
-
       if (addHostInfoResult !== host.length) {
         throw new BadGatewayException(`약속 host 데이터 추가 오류입니다`);
       }
