8e1d2297c58d0dcd63a2a399212ac666a7471018
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-09-28 14:57:33 +0900

    Add(minho/board):entity, dto 작성 #4

---
+++ main-project/src/announcements/dto/update-announcement.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+export class UpdateAnnouncementDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty({ example: 21, description: '공지사항 번호' })
+  announcementNo: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example: '김민호 취업 공지 :)',
+    description: '공지사항 제목',
+  })
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example:
+      '모던 애자일 4기 백엔드팀 김민호씨가 12월 25일 부로 카카오톡에 입사했음을 알립니다 짝짝짝~',
+    description: '공지사항 내용',
+  })
+  description: string;
+}
