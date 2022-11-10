5ebcefea3f6f5fe539beec0dbd8dbb9ffda6e374
Kim minho <90795904+klaus9267@users.noreply.github.com>
2022-11-10 18:09:09 +0900

    Update app.service.ts

--- main-project/src/app.service.ts
+++ main-project/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const str = "Hello World!"+process.env.PORT
+    return str;
   }
 }
