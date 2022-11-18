038f8534d338f0705b48022e9d2cc84576a08a32
KimSoo <ilppang@naver.com>
2022-11-18 14:07:08 +0900

    Add(hyunsoo/exception): exception filter 구현

--- main-project/src/main.ts
+++ main-project/src/main.ts
@@ -3,9 +3,12 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter()); // 전역 필터 적용
+
   const port = process.env.PORT;
   const config = new DocumentBuilder()
     .setTitle('4term project API')
