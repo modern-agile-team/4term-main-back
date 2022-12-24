f92a61dfb7aa88b4dfa04258e059637ad9176f43
JUHA <khabh@naver.com>
2022-12-24 23:13:24 +0900

    Add(juha/kakao) : 카카오 토큰으로 유저 정보 받아오기 #196

--- main-project/src/auth/auth.controller.ts
+++ main-project/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
 import { Controller } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/login/kakao')
+  async kakaoLogin(@Body('token') token: string) {
+    await this.authService.kakaoLogin(token);
+    return { success: true };
+  }
+}
--- main-project/src/auth/auth.service.ts
+++ main-project/src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async kakaoLogin(token: string) {
+    const kakaoUserInfoUrl = 'https://kapi.kakao.com/v2/user/me';
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded=utf-8',
+      Authorization: 'Bearer ' + token,
+    };
+
+    const { data } = await axios({
+      method: 'GET',
+      url: kakaoUserInfoUrl,
+      timeout: 30000,
+      headers,
+    });
+    const { email, gender } = data.kakao_account;
+  }
+}
