720ad4797ee3b7c0397935120a31f7e2ee074a89
JUHA <khabh@naver.com>
2023-01-03 11:30:17 +0900

    Feature(juha/auth) : 토큰 생성을 위한 프로필 조회 기능 구현 #196

--- main-project/src/users/interface/user.interface.ts
+++ main-project/src/users/interface/user.interface.ts
@@ -1,4 +1,12 @@
 export interface User {
   userNo: number;
   status: number;
+  accessToken?: string;
+}
+
+export interface Profile {
+  userNo: number;
+  gender: boolean;
+  nickname: string;
+  profileImage: string;
 }
---
+++ main-project/src/users/repository/user-profiles.repository.ts
@@ -0,0 +1,30 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { UserProfile } from '../entity/user-profile.entity';
+import { Profile } from '../interface/user.interface';
+
+@EntityRepository(UserProfile)
+export class UserProfilesRepository extends Repository<UserProfile> {
+  async getUserProfile(userNo: number): Promise<Profile> {
+    try {
+      const userProfile: Profile = await this.createQueryBuilder(
+        'user_profiles',
+      )
+        .leftJoin('user_profiles.profileImage', 'profileImages')
+        .select([
+          'user_profiles.userNo AS userNo',
+          'user_profiles.gender AS gender',
+          'user_profiles.nickname AS nickname',
+          'profileImages.imageUrl AS profileImage',
+        ])
+        .where('user_profiles.userNo = :userNo', { userNo })
+        .getRawOne();
+
+      return userProfile;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `${error} 유저 프로필 조회 오류(getUserProfile): 알 수 없는 서버 에러입니다.`,
+      );
+    }
+  }
+}
