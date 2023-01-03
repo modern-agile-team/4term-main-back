bc1323ac0b614d848203c869a940010935ef8952
JUHA <khabh@naver.com>
2023-01-04 07:21:26 +0900

    Feature(juha/user) : 프로필 생성 및 프로필 이미지 업로드 기능 구현 #220

--- main-project/src/users/repository/users.repository.ts
+++ main-project/src/users/repository/users.repository.ts
@@ -1,6 +1,11 @@
 import { InternalServerErrorException } from '@nestjs/common';
 import { ResultSetHeader } from 'mysql2';
-import { EntityRepository, InsertResult, Repository } from 'typeorm';
+import {
+  EntityRepository,
+  InsertResult,
+  Repository,
+  UpdateResult,
+} from 'typeorm';
 import { Users } from '../entity/user.entity';
 
 @EntityRepository(Users)
@@ -32,6 +37,36 @@ export class UsersRepository extends Repository<Users> {
     } catch (error) {
       throw new InternalServerErrorException(
         `${error} 유저 생성(createUser): 알 수 없는 서버 에러입니다.`,
+      );
+    }
+  }
+
+  async getUserByNo(userNo: number): Promise<Users> {
+    try {
+      const user = await this.createQueryBuilder('users')
+        .where('users.no = :userNo', { userNo })
+        .getOne();
+
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `${error} 유저 조회 에러(getUserByNo): 알 수 없는 서버 에러입니다.`,
+      );
+    }
+  }
+
+  async updateUserStatus(userNo: number, status: number): Promise<number> {
+    try {
+      const { affected }: UpdateResult = await this.createQueryBuilder()
+        .update(Users)
+        .set({ status })
+        .where('no = :userNo', { userNo })
+        .execute();
+
+      return affected;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `${error} 유저 상태 수정 에러(updateUserStatus): 알 수 없는 서버 에러입니다.`,
       );
     }
   }
--- main-project/src/users/users.service.ts
+++ main-project/src/users/users.service.ts
@@ -1,4 +1,93 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AwsService } from 'src/aws/aws.service';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UserProfilesRepository } from './repository/user-profiles.repository';
+import { UsersRepository } from './repository/users.repository';
+import { InternalServerErrorException } from '@nestjs/common/exceptions';
+import { ProfileImagesRepository } from './repository/profile-images.repository';
+import { UserStatus } from 'src/common/configs/user-status.config';
+import { Users } from './entity/user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private userRepository: UsersRepository,
+    private readonly userProfileRepository: UserProfilesRepository,
+    private readonly profileImageRepository: ProfileImagesRepository,
+    private readonly awsService: AwsService,
+  ) {}
+
+  async createUserProfile(
+    createProfileDto: CreateProfileDto,
+    profileImage: Express.Multer.File,
+  ) {
+    const { userNo } = createProfileDto;
+    const user = await this.getUserByNo(userNo);
+    if (user.status != UserStatus.NO_PROFILE) {
+      throw new BadRequestException(`프로필을 만들 수 없는 유저입니다.`);
+    }
+
+    const userProfileNo: number = await this.saveUserProfile(createProfileDto);
+    const imageUrl = await this.getProfileImageUrl(profileImage, userNo);
+    await this.saveProfileImage(userProfileNo, imageUrl);
+    await this.updateUserStatus(userNo, UserStatus.SHCOOL_NOT_AUTHENTICATED);
+  }
+
+  private async getProfileImageUrl(image: Express.Multer.File, userNo: number) {
+    if (!image) {
+      return null;
+    }
+
+    return await this.awsService.uploadProfileImage(image, userNo);
+  }
+
+  private async saveProfileImage(userProfileNo: number, imageUrl: string) {
+    const isProfileImageSaved: number =
+      await this.profileImageRepository.createProfileImage(
+        userProfileNo,
+        imageUrl,
+      );
+
+    if (!isProfileImageSaved) {
+      throw new InternalServerErrorException(`유저 이미지 생성 오류입니다.`);
+    }
+  }
+
+  private async saveUserProfile(
+    createProfileDto: CreateProfileDto,
+  ): Promise<number> {
+    const userProfileNo: number =
+      await this.userProfileRepository.createUserProfile(createProfileDto);
+
+    if (!userProfileNo) {
+      throw new InternalServerErrorException(`유저 프로필 생성 오류입니다.`);
+    }
+
+    return userProfileNo;
+  }
+
+  private async getUserByNo(userNo: number): Promise<Users> {
+    const user: Users = await this.userRepository.getUserByNo(userNo);
+
+    if (!user) {
+      throw new BadRequestException(`존재하지 않는 유저 번호입니다.`);
+    }
+
+    return user;
+  }
+
+  private async updateUserStatus(
+    userNo: number,
+    status: number,
+  ): Promise<void> {
+    const isStatusUpdated = await this.userRepository.updateUserStatus(
+      userNo,
+      status,
+    );
+
+    if (!isStatusUpdated) {
+      throw new InternalServerErrorException(`유저 status 수정 오류입니다.`);
+    }
+  }
+}
