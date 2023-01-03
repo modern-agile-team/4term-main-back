71bd2b404b836420a1ee160def9ee97f8018e50b
JUHA <khabh@naver.com>
2023-01-04 07:16:13 +0900

    Refactor(juha/user) : aws에 이미지 업로드하는 로직 controller에서 제거 #220

--- main-project/src/users/users.controller.ts
+++ main-project/src/users/users.controller.ts
@@ -2,29 +2,21 @@ import { Controller, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiOperation } from '@nestjs/swagger';
-import { AwsService } from 'src/aws/aws.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    private usersService: UsersService,
-    private readonly awsService: AwsService,
-  ) {}
+  constructor(private usersService: UsersService) {}
 
   @Post('/profile')
   @UseInterceptors(FileInterceptor('file'))
   @ApiOperation({ summary: '유저 프로필 생성' })
   async createUserProfile(
-    @UploadedFile() file: Express.Multer.File,
+    @UploadedFile() profileImage: Express.Multer.File,
     @Body() createProfileDto: CreateProfileDto,
   ) {
-    const result = await this.awsService.uploadProfileImage(
-      file,
-      createProfileDto.userNo,
-    );
-
+    await this.usersService.createUserProfile(createProfileDto, profileImage);
     return { success: true };
   }
 }
