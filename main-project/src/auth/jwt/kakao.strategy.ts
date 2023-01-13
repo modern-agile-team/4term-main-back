9729f657841e96a5d882e2d341fc06a4507a516c
JUHA <khabh@naver.com>
2023-01-13 21:55:45 +0900

    Add(juha/auth) : 카카오 인증 strategy 추가 #196

---
+++ main-project/src/auth/jwt/kakao.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-kakao';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class KakaoStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      clientID: configService.get<string>('KAKAO_CLIENT_ID'),
+      callbackURL: configService.get<string>('KAKAO_CALLBACK'),
+    });
+  }
+
+  async validate(accessToken, refreshToken, profile, done) {}
+}
