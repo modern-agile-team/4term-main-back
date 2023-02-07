bb6f0903dbe3e75a061ce9eb34f0ed53e75dd658
JUHA <84626225+khabh@users.noreply.github.com>
2023-02-07 14:42:31 +0900

    .

--- main-project/src/manners/manners.service.ts
+++ main-project/src/manners/manners.service.ts
@@ -1,32 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import {
   BadRequestException,
   InternalServerErrorException,
   NotFoundException,
-} from "@nestjs/common/exceptions";
-import { ResultSetHeader } from "mysql2";
-import { NoticeType } from "src/common/configs/notice-type.config";
+} from '@nestjs/common/exceptions';
+import { ResultSetHeader } from 'mysql2';
+import { NoticeType } from 'src/common/configs/notice-type.config';
 import {
   EndedMeeting,
   MeetingMember,
   SortedMembers,
-} from "src/meetings/interface/meeting.interface";
-import { MeetingRepository } from "src/meetings/repository/meeting.repository";
-import { Notices } from "src/notices/entity/notices.entity";
-import { SavedNotice } from "src/notices/interface/notice.interface";
-import { NoticesRepository } from "src/notices/repository/notices.repository";
-import { EntityManager } from "typeorm";
-import { UpdateMannerDto } from "./dto/update-manner.dto";
-import { Manners } from "./entity/manners.entity";
-import { updatedManner } from "./interface/manner.interface";
-import { MannersRepository } from "./repository/manners.repository";
+} from 'src/meetings/interface/meeting.interface';
+import { MeetingRepository } from 'src/meetings/repository/meeting.repository';
+import { Notices } from 'src/notices/entity/notices.entity';
+import { SavedNotice } from 'src/notices/interface/notice.interface';
+import { NoticesRepository } from 'src/notices/repository/notices.repository';
+import { EntityManager } from 'typeorm';
+import { UpdateMannerDto } from './dto/update-manner.dto';
+import { Manners } from './entity/manners.entity';
+import { updatedManner } from './interface/manner.interface';
+import { MannersRepository } from './repository/manners.repository';
 
 @Injectable()
 export class MannersService {
   constructor(
     private readonly mannersRepository: MannersRepository,
     private readonly noticesRepository: NoticesRepository,
-    private readonly meetingRepository: MeetingRepository
+    private readonly meetingRepository: MeetingRepository,
   ) {}
 
   private readonly NOTICE_SENDER = 1;
@@ -35,10 +35,10 @@ export class MannersService {
   async updateManner(
     senderNo: number,
     { grade, noticeNo }: UpdateMannerDto,
-    manager: EntityManager
+    manager: EntityManager,
   ): Promise<void> {
     if (grade % 0.5) {
-      throw new BadRequestException("올바르지 않은 평점 형식입니다.");
+      throw new BadRequestException('올바르지 않은 평점 형식입니다.');
     }
 
     const manner: Manners = await this.getMannerByNotice(noticeNo, senderNo);
@@ -52,7 +52,7 @@ export class MannersService {
       .updateManner(manner.userNo, updatedManner);
     if (!updateMannerResult) {
       throw new InternalServerErrorException(
-        "평점 수정(updateManner): 알 수 없는 서버 에러입니다."
+        '평점 수정(updateManner): 알 수 없는 서버 에러입니다.',
       );
     }
     await this.deleteNotice(noticeNo, manager);
@@ -60,7 +60,7 @@ export class MannersService {
 
   private async deleteNotice(
     noticeNo: number,
-    manager: EntityManager
+    manager: EntityManager,
   ): Promise<void> {
     const noticeDeleted: number = await manager
       .getCustomRepository(NoticesRepository)
@@ -68,30 +68,30 @@ export class MannersService {
 
     if (!noticeDeleted) {
       throw new InternalServerErrorException(
-        "알림 삭제(deleteNotice): 알 수 없는 서버 에러입니다."
+        '알림 삭제(deleteNotice): 알 수 없는 서버 에러입니다.',
       );
     }
   }
 
   private async getMannerByNotice(
     noticeNo: number,
-    senderNo: number
+    senderNo: number,
   ): Promise<Manners> {
     const notice: Notices = await this.noticesRepository.getNoticeByNo(
-      noticeNo
+      noticeNo,
     );
     if (!notice || notice.userNo !== senderNo) {
-      throw new NotFoundException("존재하지 않는 알림입니다.");
+      throw new NotFoundException('존재하지 않는 알림입니다.');
     }
     if (notice.type !== NoticeType.MANNER_REQUEST) {
-      throw new BadRequestException("다른 타입의 알림입니다.");
+      throw new BadRequestException('다른 타입의 알림입니다.');
     }
 
     const manner: Manners = await this.mannersRepository.getMannerByUserNo(
-      notice.targetUserNo
+      notice.targetUserNo,
     );
     if (!manner) {
-      throw new NotFoundException("탈퇴한 유저입니다.");
+      throw new NotFoundException('탈퇴한 유저입니다.');
     }
 
     return manner;
@@ -114,7 +114,7 @@ export class MannersService {
   }
 
   private getMemberCombinations(
-    meeting: EndedMeeting<MeetingMember[]>
+    meeting: EndedMeeting<MeetingMember[]>,
   ): number[][] {
     const members: SortedMembers = { guests: [], hosts: [] };
     meeting.members.forEach((member) => {
@@ -137,7 +137,7 @@ export class MannersService {
   }
 
   private createMannerNotices(
-    meetings: EndedMeeting<MeetingMember[]>[]
+    meetings: EndedMeeting<MeetingMember[]>[],
   ): SavedNotice[] {
     const notices = [];
     meetings.forEach((meeting) => {
@@ -158,7 +158,7 @@ export class MannersService {
 
   private async saveNotices(
     notices: SavedNotice[],
-    manager: EntityManager
+    manager: EntityManager,
   ): Promise<void> {
     if (!notices.length) {
       return;
@@ -168,24 +168,24 @@ export class MannersService {
       .saveNotice(notices);
 
     if (noticeSavedResult.affectedRows !== notices.length) {
-      throw new InternalServerErrorException("매너 알림 생성 오류입니다.");
+      throw new InternalServerErrorException('매너 알림 생성 오류입니다.');
     }
   }
 
   private async updateEndedMeetings(
     endedMeetings: EndedMeeting<MeetingMember[]>[],
-    manager: EntityManager
+    manager: EntityManager,
   ): Promise<void> {
     const meetings: number[] = [];
     endedMeetings.forEach((endedMeeting) =>
-      meetings.push(endedMeeting.meetingNo)
+      meetings.push(endedMeeting.meetingNo),
     );
 
     const meetingsUpdatedResult: number = await manager
       .getCustomRepository(MeetingRepository)
       .updateEndedMeetings(meetings);
     if (meetingsUpdatedResult !== meetings.length) {
-      throw new InternalServerErrorException("종료된 약속 수정 오류입니다.");
+      throw new InternalServerErrorException('종료된 약속 수정 오류입니다.');
     }
   }
 
