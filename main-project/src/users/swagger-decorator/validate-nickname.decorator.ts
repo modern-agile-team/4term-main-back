d97429639c44db6414205d300f714e58a875d3c5
JUHA <84626225+khabh@users.noreply.github.com>
2023-03-07 12:30:11 +0900

    .

--- main-project/src/users/swagger-decorator/validate-nickname.decorator.ts
+++ main-project/src/users/swagger-decorator/validate-nickname.decorator.ts
@@ -1,20 +1,19 @@
-import { applyDecorators } from '@nestjs/common';
-import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { applyDecorators } from "@nestjs/common";
+import { ApiBearerAuth, ApiOkResponse, ApiOperation } from "@nestjs/swagger";
 
-import { SwaggerApiResponse } from 'src/common/swagger/api-response.swagger';
+import { SwaggerApiResponse } from "src/common/swagger/api-response.swagger";
 
 export function ApiValidateNickname() {
   return applyDecorators(
-    ApiBearerAuth(),
     ApiOperation({
-      summary: '닉네임 중복 여부 확인',
+      summary: "닉네임 중복 여부 확인",
     }),
     ApiOkResponse(
       SwaggerApiResponse.success(
-        '사용 가능한 닉네임이면 true, 중복된 닉네임이면 false 반환',
-        '닉네임 중복 여부 확인',
-        { isValidNickname: true },
-      ),
-    ),
+        "사용 가능한 닉네임이면 true, 중복된 닉네임이면 false 반환",
+        "닉네임 중복 여부 확인",
+        { isValidNickname: true }
+      )
+    )
   );
 }
